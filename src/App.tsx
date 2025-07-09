47f8465df3589b02c8031f89a332816fa07dbfe5
julio-nt <julio.nt753@gmail.com>
2025-07-09 01:12:49 -0300

    localstorage hook em andamento

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
+import { AppButton, AppTable } from '@ntdsk/react-ui';
 import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="flex flex-col items-center min-h-screen bg-gray-100">
-      <h1 className="text-3xl font-semibold">My Web Cart</h1>
-      <button className="bg-slate-800 text-white p-2">Começar Novo Carrinho</button>
+    <div className='flex flex-col items-center min-h-screen bg-gray-100'>
+      <h1 className='text-3xl font-semibold'>My Web Cart</h1>
+      <AppButton label='Novo Carrinho' className='bg-slate-800 text-white p-2' />
+      <AppTable
+        data={[{ name: 'Leite', price: 8.9 }]}
+        columns={[
+          { header: 'Nome', accessor: 'name' },
+          { header: 'Preço', accessor: 'price' },
+          { header: 'Quantidade', accessor: 'quantity' },
+          { header: 'Total', accessor: 'total' },
+        ]}
+      />
       <Footer />
     </div>
   );
--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,3 +1,61 @@
-const useLocalStorage = () => {};
+import { useMoment } from '@ntdsk/react-ui';
+
+type CartItem = { name: string; price: number; quantity: number };
+
+type Cart = { id: number; item: CartItem[] };
+
+type LocalStorageFields = 'session' | 'name' | 'cart';
+
+type LocalStorage = {
+  session: Date;
+  name?: string;
+  cart: Cart[];
+};
+
+type GetItemResult = LocalStorage | Date | string | { id: number; item: CartItem[] }[] | undefined;
+
+const useLocalStorage = () => {
+  const { momentFn } = useMoment();
+
+  const getItem = (key?: LocalStorageFields): GetItemResult => {
+    const item = localStorage.getItem('my_web_cart');
+    const itemParsed: LocalStorage = item ? JSON.parse(item) : null;
+    if (!key || !itemParsed) return itemParsed;
+    const itemToReturn = itemParsed[key];
+    return itemToReturn;
+  };
+
+  const setItem = ({ key, value }: { key: LocalStorageFields; value: string | Date | CartItem[] }) => {
+    const localData = getItem();
+  };
+
+  const saveCartItem = (newCart: CartItem, group?: number) => {
+    const cartList = getItem('cart') ? (getItem('cart') as Cart[]) : [];
+    const currentCart = cartList.find((cart) => cart.id === group);
+    if (!currentCart || currentCart.item?.length === 0) {
+      return;
+    }
+
+    currentCart.item?.map((cart) => {
+      if (cart.name === newCart.name) {
+        return newCart;
+      }
+      return cart;
+    });
+  };
+
+  const isSessionExpired = () => {
+    const sessionTimeRaw = getItem('session') as Date;
+    if (!sessionTimeRaw) return true;
+
+    const sessionTime = momentFn(sessionTimeRaw);
+    const currentTime = momentFn();
+
+    const diffInDays = currentTime.diff(sessionTime, 'days');
+    return diffInDays >= 7;
+  };
+
+  return { isSessionExpired, getItem };
+};
 
 export default useLocalStorage;
